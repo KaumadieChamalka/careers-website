9c0b5feb33cd16388cb8c00fed4ff6753c522620
KaumadieChamalka <150341889+KaumadieChamalka@users.noreply.github.com>
2025-06-12 04:58:49 +0000

    add application form changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,12 +6,14 @@ JOBS = [{
     'id': 1,
     'title': 'Data Analyst',
     'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
+    'salary': '10,00,000',
+    'currency': 'Rs'
 }, {
     'id': 2,
     'title': 'Data Scientist',
     'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
+    'salary': '15,00,000',
+    'currency': 'Rs'
 }, {
     'id': 3,
     'title': 'Frontend Engineer',
@@ -20,19 +22,37 @@ JOBS = [{
     'id': 4,
     'title': 'Backend Engineer',
     'location': 'San Francisco, USA',
-    'salary': '$120,000'
+    'salary': '120,000',
+    'currency': '$'
 }]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name="ABC")
+    return render_template('home.html', jobs=JOBS)
 
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+    return jsonify(JOBS)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = JOBS[int(id) - 1]
+    if not job:
+        return "Not Found", 404
+    return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+    data = request.form
+    job = JOBS[int(id) - 1]
+    return render_template('application_submitted.html',
+                           application=data,
+                           job=job)
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
